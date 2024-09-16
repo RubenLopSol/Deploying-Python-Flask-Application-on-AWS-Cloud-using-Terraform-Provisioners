f6f0722141a103fd99d8cad48e53006a9a6a69fa
Ruben <rubenlopsol86@gmail.com>
2024-09-16 14:00:26 +0200

    ec2 + SG

--- Deployment/variables.tf
+++ Deployment/variables.tf
@@ -11,8 +11,30 @@ variable "vpc-cidr" {
 
 variable "subnet-cidr" {
   description = "Value for subnet CIDR"
+  type = string
 }
 
 variable "subnet-az" {
   description = "Value for subnet AZ"
+  type = string
+}
+
+variable "ami_id" {
+  description = "Value for ami_id"
+  type = string
+}
+
+variable "instance_type" {
+  description = "Value for Instance_type"
+  type = string
+}
+
+variable "key_name" {
+  description = "Value for Key name"
+  type = string
+}
+
+variable "sg-name" {
+  description = "Value for Securyti Group"
+  type = string
 }
