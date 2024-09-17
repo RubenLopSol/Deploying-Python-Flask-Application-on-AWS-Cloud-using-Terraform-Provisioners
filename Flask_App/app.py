4730ac7c04f564e0febccfbde77335593c055537
Ruben <rubenlopsol86@gmail.com>
2024-09-17 20:18:47 +0200

    app

--- Flask_App/app.py
+++ Flask_App/app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+    # host allow outside of container as well.by default is serv only in localhost inside container
+    app.run(debug=True, host='0.0.0.0', port=80)
