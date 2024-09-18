49bc525c5b242e0d4f9350001140f26e89bc4550
Ruben <rubenlopsol86@gmail.com>
2024-09-18 11:00:49 +0200

    done, until modification using ansible

--- Deployment/ec2.tf
+++ Deployment/ec2.tf
@@ -12,15 +12,17 @@ resource "aws_instance" "server" {
     host        = self.public_ip
   }
 
+  #TODO  Ansible
+
   # # Instala Docker en la instancia EC2
   # provisioner "remote-exec" {
   #   inline = [
   #     "echo 'Instalando Docker...'",
   #     "sudo apt update -y",
-  #     "sudo apt install -y docker.io",  # Instala Docker
-  #     "sudo systemctl start docker",    # Inicia Docker
-  #     "sudo systemctl enable docker",   # Activa Docker al iniciar el sistema
-  #     "sudo usermod -aG docker ubuntu", # Añade el usuario 'ubuntu' al grupo Docker
+  #     "sudo apt install -y docker.io",
+  #     "sudo systemctl start docker",
+  #     "sudo systemctl enable docker",
+  #     "sudo usermod -aG docker ec2-user",
   #   ]
   # }
 
@@ -29,8 +31,8 @@ resource "aws_instance" "server" {
   #   inline = [
   #     "echo 'Ejecutando la aplicación Flask desde Docker Hub...'",
   #     # Asegúrate de que Docker esté instalado y ejecuta el contenedor Flask
-  #     "sudo docker pull your-dockerhub-username/your-flask-app:latest",              # Cambia por tu imagen en Docker Hub
-  #     "sudo docker run -d -p 80:5000 your-dockerhub-username/your-flask-app:latest", # Corre el contenedor y expone el puerto 5000 al puerto 80 de la instancia
+  #     "sudo docker pull rubenlopsol/flask-app:v1.0.0",
+  #     "sudo docker run -d -p 80:5000 rubenlopsol/flask-app:v1.0.0",
   #   ]
   # }
 
